160e6e1a5bc913c4bcc71817d4df09fbaa70e33d
Vladimir Drizheruk <vladimir@drizheruk.com.ua>
2017-01-12 22:06:46 +0200

    build vault image

--- service/build-vault.sh
+++ service/build-vault.sh
@@ -3,14 +3,18 @@ DIR=$(dirname $(readlink -f $0))
 . ${DIR}/tools.sh
 
 IMAGE_NAME="vdrizheruk/vault"
-IMAGE_TAG="0.6.4"
+IMAGE_TAG="0.6.2"
 IMAGE=`echo "${IMAGE_NAME}:${IMAGE_TAG}" | sed -e 's/[\.\:\/&]/\\\\&/g'`
+
+cat "${DIR}/vault/Dockerfile.template" | sed -e "s/%VAULT_VERSION%/${IMAGE_TAG}/g" > "${DIR}/vault/Dockerfile"
 
 if [ 0 -lt `docker images | grep '${IMAGE}' | wc -l $1` ]; then
     docker -- pull `echo ${IMAGE} | sed -e 's/\\\\//g'`
 fi
 
 build "${DIR}/vault"  ${IMAGE_NAME} ${IMAGE_TAG}
+
+rm ${DIR}/vault/Dockerfile
 
 echo
 info "To push this image to the docker registry please run:"
